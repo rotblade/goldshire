f036794af3adf9e40d2081220d254ed7f49d6637
Zhang Gavin <gavin.zhang@alcatel-lucent.com>
2017-11-29 16:04:09 +0800

    a bit of code

--- goldshire/investment.py
+++ goldshire/investment.py
@@ -48,8 +48,42 @@ class Stocks:
         grp_trade = trades.groupby([trades.index, trades['Transaction']])
         grp_dividend = dividends.groupby(dividends.index)
 
+        stocks = trades[['Name']].drop_duplicates()
+
         # Claculate total bought/sold qty for each stock.
-        qty_sum = grp_trade['Qty'].sum().unstack(fill_value=0)
-        #df = self._stocks.join(qty_sum)
-        #df.columns = ['Name', 'Hold', 'B_Qty', 'S_Qty']
-        #df['Qty'] = df['B_Qty'] + df['S_Qty']
+        bs_sum = grp_trade['Qty'].sum().unstack(fill_value=0)
+        df = stocks.join(bs_sum)
+        df.columns = ['Name', 'B_Qty', 'S_Qty']
+        df['Qty'] = df['B_Qty'] + df['S_Qty']
+
+        # Calculate average bought/sold price.
+        basis_sum = grp_trade['Basis'].sum().unstack(fill_value=0)
+        df['B_Cost'] = basis_sum['BUY'] / df['B_Qty']
+        df['S_Cost'] = basis_sum['SELL'] / df['S_Qty'].abs()
+
+        # Calculate dividend for each stock.
+        df['Dividend'] = grp_dividend['Dividend'].sum()
+        df['Dividend'].fillna(0.0, inplace=True)
+
+        # Calculate realized profit/loss
+        sold_cost = df['B_Cost'] * df['S_Qty'].abs()
+        df['R_PnL'] = basis_sum['SELL'] - sold_cost
+        df['R_PnL'].fillna(0.0, inplace=True)
+
+        # Calcuate unrealized profit/loss
+        hold = df.loc[lambda df: df.Qty > 0]
+        df['Last'] = get_lastprice(hold.index, self.csvpath+'history/')
+        df['UR_PnL'] = df['Qty'] * (df['Last'] - df['B_Cost'])
+        df['UR_PnL'].fillna(0.0, inplace=True)
+
+        # Calculate total earning for each stock.
+        df['Earning'] = df['Dividend'] + df['R_PnL'] + df['UR_PnL']
+
+        # Calculate return for each stock.
+        df['Return'] = df['Earning'] / (df['B_Qty'] * df['B_Cost'])
+
+
+        # Add currency info
+        df['Currency'] = self.currency
+
+        return df
