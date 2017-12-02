43d10923e0a4d8113c273a4b86f872659c078d92
Zhang Gavin <rotblade@163.com>
2017-12-03 00:15:08 +0800

    Stocks class test ok.

--- goldshire/investment.py
+++ goldshire/investment.py
@@ -12,11 +12,12 @@ class Invest:
     Represent long term investment in one base currency.
     '''
 
-    def __init__(self, currency, fund, stocks, start=datetime.date(2016,1,1)):
+    def __init__(self, currency, fund, stocks, start=datetime.date(2016, 1, 1)):
         self.currency = currency.upper()
         self.start = start
-        fund = pd.read_csv(csvpath+fund, parse_dates=['Date'], index_col=0)
+        fund = pd.read_csv(csvpath + fund, parse_dates=['Date'], index_col=0)
         c = CurrencyRates()
+
         def c2BCurrency(r):
             if r.Currency.upper() == self.currency:
                 return r.Amount
@@ -28,6 +29,8 @@ class Invest:
         self._stocks = stocks
         self.initial = fund.loc[:start]['BaseAmount'].sum()
 
+    def __repr__(self):
+        return (f'{slef.__class__.__name__} - {self.currency}')
 
     def getInvest(self, end=datetime.date.today()):
         capital = self._fund.loc[:end]['BaseAmount'].sum()
@@ -38,7 +41,7 @@ class Invest:
             'commission': 0.0,
             'tax': 0.0,
         }
-            
+
         value = self.initial
         for s in self._stocks:
             df = s.getStocks(end)
@@ -68,8 +71,7 @@ class Stocks:
     '''
 
     def __init__(self, currency, records):
-        self.currency = currency
-        self._csvpath = csvpath
+        self.currency = currency.upper()
 
         trades = pd.read_csv(records[0], parse_dates=['Date'],
                              dtype={'Symbol': str, 'Qty': np.int64})
@@ -81,101 +83,103 @@ class Stocks:
             trades['Transaction'] == 'BUY', proceed + fee, proceed - fee)
         self._trades = trades
 
-        dividends = csv2df(records[1]).set_index('Symbol')
+        dividends = pd.read_csv(records[1], parse_dates=['Date'],
+                                dtype={'Symbol': str, 'Qty': np.int64})
+        dividends.set_index('Symbol', inplace=True)
         fee = dividends['Commission'] + dividends['Tax']
         dividends['Dividend'] = dividends['PerShare'] * dividends['Qty'] - fee
         self._dividends = dividends
 
 
-    def _selectRows(self, end):
-        until = pd.Timestamp(end)
-
-        trades = self._trades.loc[lambda df: df.Date <= until]
-        dividends = self._dividends.loc[lambda df: df.Date <= until]
-        grp_trade = trades.groupby([trades.index, trades['Transaction']])
-        stocks = trades[['Name']].drop_duplicates()
-        bs_sum = grp_trade['Qty'].sum().unstack(fill_value=0)
-        df = stocks.join(bs_sum)
-        df.columns = ['Name', 'B_Qty', 'S_Qty']
-        df['Qty'] = df['B_Qty'] + df['S_Qty']
-
-        return df
+    def __repr__(self):
+        return (f'{self.__class__.__name__}-{self.currency}: {len(self._trades)} trades & {len(self._dividends)} dividends')
 
 
-    def _getTradefee(self, end):
-        until = pd.Timestamp(end)
-
-        trades = self._trades.loc[lambda df: df.Date <= until]
-        grp_trade = trades.groupby(trades.index)
-
-        return grp_trade[['Commission', 'Tax']].sum()
-
-
-    def _getDividendfee(self, end):
-        until = pd.Timestamp(end)
-
-        dividends = self._dividends.loc[lambda df: df.Date <= until]
-        grp_dividend = dividends.groupby(dividends.index)
-
-        return grp_dividend[['Commission', 'Tax']].sum()
-
-
-    def getPrice(self, symbols, day=datetime.date.today()):
+    @staticmethod
+    def getPrice(csvpath, symbols, day=datetime.date.today()):
         prices = []
         for symbol in symbols:
-            df = pd.read_csv(join(self._csvpath+'historic/', symbol + '.csv'),
-                             names=['date', 'price'], parse_dates=['date']
+            df = pd.read_csv(join(csvpath + 'historic/', symbol + '.csv'),
+                             names=['date', 'price'], parse_dates=['date'],
                              index_col=0)
             if day not in df.index:
-                day_range = pd.date_range(day-DateOffset(days=10),
-                                          day-DateOffset(days=1))
+                day_range = pd.date_range(day - DateOffset(days=10),
+                                          day - DateOffset(days=1))
                 isFound = False
-                for d in day_range:
+                for d in day_range[::-1]:
                     if d in df.index:
                         day = d
                         isFound = True
-                        break;
+                        break
                 if not isFound:
-                    raise KeyError('No price found for specific date!')
+                    raise KeyError(f'No price found for {symbol} on {day}')
 
             prices.append(df.loc[day, 'price'])
 
         return pd.Series(prices, index=symbols)
 
 
-    def getHolding(self, end=datetime.date.today()):
-        df = self._selectRows(end)
-        return df[['Name', 'Qty']].loc[lambda df: df.Qty > 0]
+    def _preCal(self, end):
+        until = pd.Timestamp(end)
+
+        trades = self._trades.loc[lambda df: df.Date <= until]
+        dividends = self._dividends.loc[lambda df: df.Date <= until]
+        grp_trade = trades.groupby([trades.index, trades['Transaction']])
+        stocks = trades[['Name']].drop_duplicates()
+        bs_sum = grp_trade['Qty', 'Basis'].sum().unstack(fill_value=0)
+        bs_sum.columns = ['B_Qty', 'S_Qty', 'B_Basis', 'S_Basis']
+        df = stocks.join(bs_sum)
+        df['Qty'] = df['B_Qty'] + df['S_Qty']
+        df['B_Cost'] = df['B_Basis'] / df['B_Qty']
+        df['S_Cost'] = df['S_Basis'] / df['S_Qty'].abs()
+
+        return df
 
 
-    def getStocks(self, end=datetime.date.today()):
-        df = self._selectRows(end)
+    def _getTrade(self, df, end):
+        until = pd.Timestamp(end)
 
-        # Add commission & tax
-        df = df.join(self._getTradefee(end))
-        df = df.join(self._getDividendfee(end), lsuffix='_t', rsuffix='_d')
-        df[['Commission_d', 'Tax_d']].fillna(0.0, inplace=True)
-
-        # Calculate average bought/sold price.
-        basis_sum = grp_trade['Basis'].sum().unstack(fill_value=0)
-        df['B_Cost'] = basis_sum['BUY'] / df['B_Qty']
-        df['S_Cost'] = basis_sum['SELL'] / df['S_Qty'].abs()
-
-        # Calculate dividend for each stock.
-        grp_dividend = dividends.groupby(dividends.index)
-        df['Dividend'] = grp_dividend['Dividend'].sum()
-        df['Dividend'].fillna(0.0, inplace=True)
+        # Add Commission & Tax fields
+        trades = self._trades.loc[lambda df: df.Date <= until]
+        grp_trade = trades.groupby(trades.index)
+        df = df.join(grp_trade['Commission', 'Tax'].sum())
 
         # Calculate realized profit/loss
         sold_cost = df['B_Cost'] * df['S_Qty'].abs()
-        df['R_PnL'] = basis_sum['SELL'] - sold_cost
+        df['R_PnL'] = df['S_Basis'] - sold_cost
         df['R_PnL'].fillna(0.0, inplace=True)
 
         # Calcuate unrealized profit/loss
         hold = df.loc[lambda df: df.Qty > 0]
-        df['Last'] = self.getPrice(hold.index, str(end_date))
+        df['Last'] = Stocks.getPrice(csvpath, hold.index, end)
         df['UR_PnL'] = df['Qty'] * (df['Last'] - df['B_Cost'])
         df['UR_PnL'].fillna(0.0, inplace=True)
+
+        return df
+
+
+    def _getDividend(self, end):
+        until = pd.Timestamp(end)
+
+        dividends = self._dividends.loc[lambda df: df.Date <= until]
+        grp_dividend = dividends.groupby(dividends.index)
+        divids = grp_dividend['Commission', 'Tax', 'Dividend'].sum()
+
+        return divids
+
+
+    def getHolding(self, end=datetime.date.today()):
+        df = self._preCal(end)
+        return df[['Name', 'Qty']].loc[lambda df: df.Qty > 0]
+
+
+    def getStocks(self, end=datetime.date.today()):
+        s_df = self._preCal(end)
+        df = self._getTrade(s_df, end)
+        df = df.join(self._getDividend(end), lsuffix='_t', rsuffix='_d')
+        df['Commission_d'].fillna(0.0, inplace=True)
+        df['Tax_d'].fillna(0.0, inplace=True)
+        df['Dividend'].fillna(0.0, inplace=True)
 
         # Calculate total earning for each stock.
         df['Earning'] = df['Dividend'] + df['R_PnL'] + df['UR_PnL']
---
+++ goldshire/tests/test_invest.py
@@ -0,0 +1,58 @@
+import unittest
+import datetime
+from config import *
+from investment import Stocks
+
+
+class TestStocks(unittest.TestCase):
+    def setUp(self):
+        self.before = datetime.date(2017, 2, 1)
+        self.today = datetime.date.today()
+        self.pfl = {}
+        for k, v in stocks.items():
+             self.pfl[k] = Stocks(k, stocks[k])
+
+    def test_new(self):
+        for v in self.pfl.values():
+            self.assertTrue(len(v._trades) > 0)
+            self.assertTrue(len(v._dividends) > 0)
+
+    def test_preCal(self):
+        df = self.pfl['hkd']._preCal(self.before)
+        fields = ['Name', 'Qty', 'B_Cost']
+        for field in fields:
+            self.assertTrue(field in df.columns)
+
+    def test_getTrade(self):
+        s_df = self.pfl['cny']._preCal(self.before)
+        df = self.pfl['cny']._getTrade(s_df, self.before)
+        fields = ['R_PnL', 'Last', 'UR_PnL', 'Commission', 'Tax']
+        for field in fields:
+            self.assertTrue(field in df.columns)
+
+    def test_getDividend(self):
+        df = self.pfl['cny']._getDividend(self.before)
+        fields = ['Commission', 'Tax', 'Dividend']
+        for field in fields:
+            self.assertTrue(field in df.columns)
+
+    def test_getPrice(self):
+        symbols = ['000895', '600585', '600660']
+        prices = Stocks.getPrice(csvpath, symbols, self.before)
+        self.assertEqual(prices.iloc[0], 21.40)
+        self.assertEqual(prices.iloc[1], 19.93)
+        self.assertEqual(prices.iloc[2], 18.80)
+
+    def test_getHolding(self):
+        holding = self.pfl['hkd'].getHolding(self.today)
+        self.assertTrue(len(holding) > 0 and len(holding) < 10)
+
+    def test_getStocks(self):
+        stocks = self.pfl['cny'].getStocks(self.before)
+        fields = ['Commission_t', 'Tax_t', 'Commission_d', 'Tax_d', 'Earning', 'Return']
+        for field in fields:
+            self.assertTrue(field in stocks.columns)
+
+
+if __name__ == '__main__':
+    unittest.main()
